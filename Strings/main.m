99681f970a7568847e4a102deeaffd0203c985a7
Jason Harrison <jasonharrisonvancouver@gmail.com>
2018-12-30 21:55:12 -0800

    madlibs

--- Strings/main.m
+++ Strings/main.m
@@ -26,6 +26,16 @@ int main(int argc, const char * argv[]) {
         NSLog(@"appended: %@\n", nameLonger);
         NSLog(@"replaced: %@\n", nameReplaced);
 
+        
+        NSString *madLib = @"Yesterday, _PERSON_ and I when to the park. On our way to the _ADJECTIVE_1 park, we saw a _ADJECTIVE_2 _NOUN_ on a bike. We also saw big _ADJECTIVE_2 balloons tied to the _NOUN_. Once we got to the _ADJECTIVE_1 park, the sky turned _ADJECTIVE_2. It started to _VERB_. _PERSON_ and I _VERB_ all the way home.";
+        
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_PERSON_" withString:@"richard feynman"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_ADJECTIVE_1" withString:@"stupid"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_ADJECTIVE_2" withString:@"hot"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_NOUN_" withString:@"frying pan"];
+        madLib = [madLib stringByReplacingOccurrencesOfString:@"_VERB_" withString:@"whistled"];
+        
+        NSLog(@"%@", madLib);
     }
     return 0;
 }
